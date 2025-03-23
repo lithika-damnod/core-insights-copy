2e002a6549a95bcf26cda8ba4787a2965444a2d4
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2025-03-24 03:18:02 +0530

    added local ip address as a allowed host in the settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%=%!0105jt&3lj%i!vee@n=8(ts3)+qj1c1107kgt5dktqtnkh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.8.167']
 
 
 # Application definition
